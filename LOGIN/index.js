92e306c9827a33b4d4f8462199842facbdaf027a
brendateruel2023 <127778293+brendateruel2023@users.noreply.github.com>
2024-03-01 22:09:35 -0300

    Update index.js

--- LOGIN/index.js
+++ LOGIN/index.js
@@ -1,11 +1,4 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var usuario = document.getElementById('usuario').value;
-    var senha = document.getElementById('senha').value;
-
-    if (usuario === '' || senha === '') {
-        alert('Preencha todos os campos.');
-        return;
-    }
 });
